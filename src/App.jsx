ac44b51a3df8bbbe76d956ca4638ec771fa59f4a
UZBPRIMEBOY <osimjonhusanboyev43@gmail.com>
2025-07-25 12:09:03 +0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-
-import './App.css'
-import Atir from './assets/components/Atir'
-import Bonus from './assets/components/Bonus'
-import Footer from './assets/components/Footer'
-import Hedaer from './assets/components/Header'
-import Mushfig from './assets/components/Mushfig'
-import Navbar from './assets/components/Navbar'
-import Navcontent from './assets/components/Navcontent'
+import "./App.css";
+import Atir from "./components/Atir";
+import Bonus from "./components/Bonus";
+import Footer from "./components/Footer";
+import Hedaer from "./components/Header";
+import Mushfig from "./components/Mushfig";
+import Navbar from "./components/Navbar";
+import Navcontent from "./components/Navcontent";
 
 function App() {
-
   return (
     <>
-     <Navbar/>
-     <Navcontent/>
-     <Hedaer/>
-     <Atir/>
-     <Bonus/>
-     <Mushfig/>
-     <Footer/>
+      <Navbar />
+      <Navcontent />
+      <Hedaer />
+      <Atir />
+      <Bonus />
+      <Mushfig />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,56 @@
+function Footer() {
+  return (
+    <>
+      <div className="Footer">
+        <div className="footertop">
+          <img src="./img/footerlogo.png" alt="" />
+        </div>
+        <div className="Footer-content">
+          <div className="footerbal">
+            <div className="loc">
+              <div className="local">
+                <img src="./img/local.png" alt="" />
+                <p>г. Астрахань: ул. Свердлова, 106;ул. Победы 55А, пав. 10.</p>
+              </div>
+              <div className="local">
+                <img src="./img/clock.png" alt="" />
+                <p>г. Астрахань: ул. Свердлова, 106;ул. Победы 55А, пав. 10.</p>
+              </div>
+            </div>
+
+            <div className="teleg">
+              <div className="watsap">
+                <img src="./img/watsap.png" alt="" />
+                <p>Мы в Whatsapp</p>
+              </div>
+              <div className="watsap">
+                <img src="./img/telegram.png" alt="" />
+                <p>Мы в Telegram</p>
+              </div>
+              <div className="watsap">
+                <img src="./img/sms.png" alt="" />
+                <p>parfumpomotivam@gmail.com</p>
+              </div>
+            </div>
+
+            <div className="bino">
+              <div className="uy">
+                <img src="./img/built.png" alt="" />
+                <p>
+                  ИП РАГИМОВА А.М.К. <br /> ИНН 510704693888
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="footerbot">
+            <p>
+              © Parfumpomotivam 2023 <br />
+              Политика конфиденциальности
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
